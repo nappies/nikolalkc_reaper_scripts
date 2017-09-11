45889a0c6ca8a6ebae8dd92a6cdbaa416cec2d1a
nikolalkc <nikolalkc@hotmail.com>
2017-09-11 13:31:47 +0200

    Type in HOPA NAME v1.6, multiple items naming

--- SNF - Type in Hopa Name.lua
+++ SNF - Type in Hopa Name.lua
@@ -9,6 +9,8 @@
  
 --[[
  * Changelog:
+ * v1.6 (2017-09-11)
+	+ Added support for naming multiple selected empty items with indexes
  * v1.5 (2017-09-05)
 	+ last_scene.dat is loaded and saved to environmental variable path %HOPA_NAME% which is set from audio manager
  * v1.4 (2017-06-02)
@@ -40,20 +42,17 @@ file_name = ""
 cur_name = nil
 answer_to_save = ""
 last_scene = ""
+empty_items = {}
+array_naming = false
 
 --MAIN FUNCTION
 function Main()
-	--get info
-	--Msg("-------------")
-	
-	
-	
 	--read environmental variable
 	hopa_name_path = os.getenv("HOPA_NAME")
 	--Msg(hopa_name_path)
 	
 
-	--procitaj fajl ako ga ima
+	--PROCITAJ FAJL U KOM JE SACUVANO IME PRETHODNOG IMENOVANJA +++++++++++++++++++++++++
 	file_path = [[]]..hopa_name_path..[[\last_scene.dat]]
 	file = io.open(file_path, "r")
 	if file ~= nil then
@@ -67,16 +66,83 @@ function Main()
 	end
 	
 	
-	cur_item = reaper.GetSelectedMediaItem(0,0)
-	cur_take = reaper.GetMediaItemTake(cur_item, 0)
-	if cur_take ~= nil then
-		cur_name = reaper.GetTakeName(cur_take)
+	cur_name = ""
+	
+	--MANAGE NAMING, SINGLE OR ARRAY==========================================================================
+	--count selected items
+	reaper.Main_OnCommand(40296,0) --select all tracks
+	selected_count = reaper.CountSelectedMediaItems(0)
+	
+	idx = 0
+	if selected_count > 1 then
+		--nadji sve empty iteme
+		for i = 0, selected_count - 1 do 
+			--get info
+			cur_item = reaper.GetSelectedMediaItem(0,i)
+			cur_take = reaper.GetMediaItemTake(cur_item, 0)
+			if cur_take == nil then
+				--ovaj je prazan, dodaj ga u niz praznih
+				empty_items[idx] = cur_item
+				idx = idx + 1
+			else
+				--nije prazan, preskoči
+			end
+		
+		end
+		
+		--SETUJ DA TREBA NIZ DA SE IMENUJE
+		if idx > 1 then 
+			--NIZ U KOME IMA VIŠE EMPTY ITEMA
+			array_naming = true
+			item_is_empty = true
+		else 
+			--NIZ U KOJEM IMA SAMO JEDAN ili NULA EMPTY ITEMa
+			--Msg("šta se dešava ako selektujemo više, a ima samo jedan empty item??*????")
+			if idx == 1 then
+				cur_item = empty_items[0]   -- kad ima jedan empty item
+			else 
+				cur_item = reaper.GetSelectedMediaItem(0,0) -- samo prvi, ili sve ????
+			end
+		end
+	
 	else
-		item_is_empty = true
-		cur_name =  reaper.ULT_GetMediaItemNote( cur_item )
+		--TREBA SAMO SELEKTOVANI ITEM DA SE IMENUJE, BIO ON TAKE ILI EMPTY ITEM, KASNIJE SE BIRA KOJI
+		empty_items[0] = reaper.GetSelectedMediaItem(0,0)
+		cur_item = empty_items[0]
+	end
+	--================================================================================================
+	
+	--IMENOVANJE NIZA SELEKTOVANIH ITEMA
+	if array_naming == true then 
+		first_item = empty_items[0]
+		input_name = reaper.ULT_GetMediaItemNote( first_item )
+		user_input,final_name = ShowDialogForNaming(input_name)
+		for i = 0, idx -1 do 
+			SetNameForItem(empty_items[i], user_input, final_name, true,i)
+		end
+	else
+		--IMENOVANJE SAMO JEDNOG SELEKTOVANOG ITEMa
+	
+		cur_take = reaper.GetMediaItemTake(cur_item, 0)
+		
+		item_is_empty = false
+		if cur_take == nil then
+			--setuj da treba empty item da se imenuje
+			item_is_empty = true
+			cur_name =  reaper.ULT_GetMediaItemNote( cur_item )
+		else
+			--setuj da treba običan tejk da imenuje
+			cur_name = reaper.GetTakeName(cur_take)
+		end
+	
+		user_input,final_name = ShowDialogForNaming(cur_name)
+		SetNameForItem(cur_item, user_input, final_name, item_is_empty)
 	end
 	
-	--split current name
+end
+
+function ShowDialogForNaming(cur_name)
+--split current name
 	if cur_name ~= nil then
 		if cur_name ~= "" then
 			scene_name, file_name = cur_name:match("([^,]+):([^,]+)")
@@ -135,14 +201,27 @@ function Main()
 		end
 	end
 	
-	
-	-- SETNAMES
+	return retval,final_name
+end
+
+
+-- SETNAMES
+function SetNameForItem(cur_item,retval,final_name,item_is_empty,index_to_add)
+	reaper.Main_OnCommand(40289,0)--unselect all items
 	if retval == true then
 		if item_is_empty ~= true then
 			reaper.GetSetMediaItemTakeInfo_String(cur_take, "P_NAME", final_name, true)
 		else
+			if index_to_add ~= nil and final_name ~= nil then
+				index_to_add = index_to_add + 1
+				zero = ""
+				if index_to_add <= 9 then 
+					zero = "0"
+				end
+				final_name = final_name.."_"..zero..index_to_add
+			end
 			reaper.ULT_SetMediaItemNote( cur_item, final_name)
-			reaper.Main_OnCommand(reaper.NamedCommandLookup("_RSb428746958e98560bf16fdec0d9022a5b13465c0"),0) -- fit notes stretch
+			reaper.Main_OnCommand(reaper.NamedCommandLookup("_RSb428746958e98560bf16fdec0d9022a5b13465c0"),0) -- fit notes stretch (ovo radi  na sve selektovane iteme)
 		end
 		
 		--Msg(file_path)
@@ -150,7 +229,7 @@ function Main()
 		file:write(answer_to_save)
 		file:close()
 	end
-	
+
 end
 
 --RUN
