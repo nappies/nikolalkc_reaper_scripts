66e7c5a4bd2e9688df24cbc257a49844da624483
nikolalkc <nikolalkc@hotmail.com>
2017-10-14 17:22:07 +0200

    relative mousewheel gain 1.0

--- SNF - Relative-mousewheel Gain.lua
+++ SNF - Relative-mousewheel Gain.lua
@@ -1,19 +1,63 @@
-function run()
-	is_new,name,sec,cmd,rel,res,val = reaper.get_action_context()
-	if is_new then
-		--reaper.ShowConsoleMsg(name .. "\nrel: " .. rel .. "\nres: " .. res .. "\nval = " .. val .. "\n")
-		item = reaper.GetSelectedMediaItem( 0,0 )
-		vol = reaper.GetMediaItemInfo_Value(item, "D_VOL")
-		--reaper.ShowConsoleMsg(vol.."\n")
-		if val > 0 then
-			local new_vol = vol*1.2
-			 reaper.SetMediaItemInfo_Value(item, "D_VOL", new_vol )
-		else
-			local new_vol = vol/1.2
-			reaper.SetMediaItemInfo_Value( item, "D_VOL", new_vol )
-		end
-	end
-	reaper.Main_OnCommand(40441,0) --rebuild peaks
+--[[
+ * ReaScript Name: SNF - RELATIVE MOUSEWHEEL GAIN
+ * Description: Changes gain to selected items with scroll of mouse
+ * Author: nikolalkc
+ * REAPER: 5.52
+ * Version: 1.0
+]]
+ 
+--[[
+ * Changelog:
+ * v1.0 (2017-10-14)
+	+ Initial Release
+]]
+
+--[[ ----- DEBUGGING ===>
+
+]]-- <=== DEBUGGING -----
+
+--UTILITIES
+function Msg(param)
+  reaper.ShowConsoleMsg(tostring(param).."\n")
 end
 
-run()
+function run()
+	is_new,name,sec,cmd,rel,res,val = reaper.get_action_context()
+	selected_count = reaper.CountSelectedMediaItems(0)
+	if selected_count <= 1 then
+		--reaper.Main_OnCommand(40529,0) --select item under mouse leaving other items selected
+		reaper.Main_OnCommand(40528,0) --select item under mouse 
+	end
+	
+	if selected_count > 0 then
+		for i = 0, selected_count - 1 do 
+			if is_new then
+				--reaper.ShowConsoleMsg(name .. "\nrel: " .. rel .. "\nres: " .. res .. "\nval = " .. val .. "\n")
+				local item = reaper.GetSelectedMediaItem( 0,i )
+				
+				if item ~= nil then
+					vol = reaper.GetMediaItemInfo_Value(item, "D_VOL")
+					--reaper.ShowConsoleMsg(vol.."\n")
+					if val > 0 then --ako je plus
+						local new_vol = vol*1.2
+						 reaper.SetMediaItemInfo_Value(item, "D_VOL", new_vol )
+					else --ako je minus
+						local new_vol = vol/1.2
+						reaper.SetMediaItemInfo_Value( item, "D_VOL", new_vol )
+					end
+				end
+			end
+		end
+	end
+	
+	--reaper.Main_OnCommand(40441,0) --rebuild peaks
+end
+
+
+
+reaper.Undo_BeginBlock()
+reaper.PreventUIRefresh( 1 )
+run() -- odradi funkciju
+reaper.PreventUIRefresh( -1 )
+reaper.Undo_EndBlock("Mousewheel Item Gain", -1)
+reaper.UpdateArrange()
