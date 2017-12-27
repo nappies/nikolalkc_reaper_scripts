21df459b07e07e65a40e6842450e240272af65fe
nikolalkc <nikolalkc@hotmail.com>
2017-12-27 13:38:31 +0100

    nove izmene test

--- SNF - Filename.lua
+++ SNF - Filename.lua
@@ -5,4 +5,5 @@ take = reaper.GetActiveTake(item)
 src = reaper.GetMediaItemTake_Source(take)
 filename = reaper.GetMediaSourceFileName(src, 1)
 reaper.ShowConsoleMsg("\n"..filename)
-
+--proba komitovanja iz atoma
+--nove izmene
