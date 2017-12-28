3a131e233e4e018e7ddbd7404347a367bd68d8ab
nikolalkc <nikolalkc@hotmail.com>
2017-12-28 12:29:31 +0100

    VELIKI RENAME

--- SNF - Get All FX Params.lua
+++
@@ -1,8 +0,0 @@
-
-track = reaper.GetSelectedTrack( 0, 0 )
-retval, minvalOut, maxvalOut = reaper.TrackFX_GetParam( track, 0, 0 )
-
-
-reaper.ShowConsoleMsg(retval.."\n")
-reaper.ShowConsoleMsg(minvalOut.."\n")
-reaper.ShowConsoleMsg(maxvalOut.."\n")
