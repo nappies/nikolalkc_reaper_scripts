3bebbbdd5489d84bb39d37a718871074e155e749
nikolalkc <nikolalkc@hotmail.com>
2017-12-28 12:50:01 +0100

    novi id za ovu skriptu

--- nikolalkc_Select Next Track For Importing.lua
+++ nikolalkc_Select Next Track For Importing.lua
@@ -1,4 +1,4 @@
-reaper.Main_OnCommand(reaper.NamedCommandLookup("_RSc8a9b735b6eea09fd7a98f1579382c170ebbe5e5"),0) --Script: SNF - MoveEditCursorToItemStart.lua
+reaper.Main_OnCommand(reaper.NamedCommandLookup("_RS025564f5f1584ea3a57161e14b1362a00d4313c9"),0) --Script: nikolalkc - Move Edit Cursor To Item Start.lua
 reaper.Main_OnCommand(reaper.NamedCommandLookup("_SWS_SELTRKWITEM"),0) --SWS: Select only track(s) with selected item(s)
 reaper.Main_OnCommand(reaper.NamedCommandLookup("_XENAKIOS_SELNEXTTRACK"),0) --Xenakios/SWS: Select next tracks
 reaper.Main_OnCommand(40289,0) --Track: Unselect all items
