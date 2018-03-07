74ae93e11a0374d08534d078d3944c7761b5b7ff
nikolalkc <nikolalkc@hotmail.com>
2018-03-07 16:53:36 +0100

    render matrix demo test

--- MHG/nikolalkc_MHG - Export Selected Sounds.lua
+++ MHG/nikolalkc_MHG - Export Selected Sounds.lua
@@ -434,7 +434,8 @@ function Main()
 		--export
 		reaper.Main_OnCommand(40717,0) --Item: Select all items in current time selection
 		selected_count = reaper.CountSelectedMediaItems(0)
-		render_selected_items()
+		create_regions_and_sort_them()
+		add_regions_to_render_queue()
 		post_export_dialog()
 
 	else --no time selection
@@ -446,7 +447,12 @@ function Main()
 			post_export_dialog("No Items Selected!")
 		else --if at least one item selected
 			--export
-			render_selected_items()
+			reaper.Main_OnCommand(40290,0) --Time selection: Set time selection to items
+			start_time, end_time =  reaper.GetSet_LoopTimeRange2( 0, false, false, 0, 0, false)
+			delta_time = end_time - start_time
+			
+			create_regions_and_sort_them()
+			add_regions_to_render_queue()
 			post_export_dialog()
 		end
 	end
@@ -455,9 +461,15 @@ function Main()
 end
 
 --other fun=================================================================================================================================================================
-function render_selected_items()
+function create_regions_and_sort_them()
+	reaper.Main_OnCommand(reaper.NamedCommandLookup("_BR_SAVE_SOLO_MUTE_ALL_ITEMS_SLOT_1"),0) --SWS/BR: Save all items' mute state, slot 01
+	reaper.Main_OnCommand(41229,0) -- save selection set #01
+	reaper.Main_OnCommand(40182,0) -- Item: Select all items
+	reaper.Main_OnCommand(40719,0) -- Item properties: Mute
+	reaper.Main_OnCommand(41239,0) -- Selection set: Load set #01
+	
 	for i = 0, selected_count - 1 do
-
+	
 		--get stuff from item
 		item[i] = reaper.GetSelectedMediaItem(0,i)
 		take[i] = reaper.GetMediaItemTake(item[i], 0)
@@ -488,7 +500,8 @@ function render_selected_items()
 	--Msg("Clip Groups:"..cg_index)
 	reaper.Main_OnCommand(40289,0) --Item: Unselect all items
 
-	--run through new array and add items to render queue
+	--run through new array and create and sort regions by level
+	dictionary = {}
 	for i = 0, cg_index - 1 do
 		reaper.SetMediaItemSelected( clip_group[i], 1 )
 		reaper.Main_OnCommand(40290,0) --Time selection: Set time selection to items
@@ -497,139 +510,154 @@ function render_selected_items()
 		--get stuff
 		local item_pos = reaper.GetMediaItemInfo_Value(clip_group[i],"D_POSITION")
 		local item_len = reaper.GetMediaItemInfo_Value(clip_group[i],"D_LENGTH")
-		local group_id = reaper.GetMediaItemInfo_Value(clip_group[i],"I_GROUPID")
+		-- local group_id = reaper.GetMediaItemInfo_Value(clip_group[i],"I_GROUPID")
 		local take = reaper.GetMediaItemTake(clip_group[i], 0)
 		local item_end = item_pos + item_len
 		local name = ""
-		if take ~= nil then
+		if take ~= nil then		--if regular item
 			name =  reaper.GetTakeName(take)
-		else
+		else	--if emtpy item
 			name = reaper.ULT_GetMediaItemNote( clip_group[i])
 			name = string.gsub (name, "\n", "")
 			name = string.gsub (name, "\r", "")
 		end
+		
 		local changed_name = string.sub(name, 2)
-		local new_name = string.gsub(changed_name, "%:", "-") --replace : with -
+		local value = string.gsub(changed_name, "%:", "-") --replace (:) with (-)
+		local new_name = "*"..value	--add specific prefix
 		local red = reaper.ColorToNative(255,0,0)|0x1000000
 
 
 		--create region with same name as clip group
 		local marker_index = reaper.AddProjectMarker2( 0, 1, item_pos, item_end, new_name, 0, red )
-
-
-		--save vertical mute state and mute all items which should not be rendered
-			reaper.Main_OnCommand(40717,0) --Item: Select all items in current time selection
-			sel_count = reaper.CountSelectedMediaItems(0)
-
-			-- get time selection start and end time
-			_start_time, _end_time =  reaper.GetSet_LoopTimeRange2( 0, false, false, 0, 0, false)
-
-			--take info from all items in vertical selection
-			local mono_item = true
-			for j = 0, sel_count - 1 do
-
-				--get stuff
-				local _item = reaper.GetSelectedMediaItem(0,j)
-				local _take = reaper.GetMediaItemTake(_item, 0)
-				local _name = ""
-				if _take ~= nil then
-					_name =  reaper.GetTakeName(_take)
-				else
-					_name = reaper.ULT_GetMediaItemNote(_item)
-					_name = string.gsub (_name, "\n", "")
-					_name = string.gsub (_name, "\r", "")
-				end
-
-				local _item_pos = reaper.GetMediaItemInfo_Value(_item,"D_POSITION")
-				local _item_len = reaper.GetMediaItemInfo_Value(_item,"D_LENGTH")
-				local _item_end = _item_pos + _item_len
-				local _current_group_id = reaper.GetMediaItemInfo_Value(_item,"I_GROUPID")
-
-				--INACTIVE -- if overlap item
-				--INACTIVE--if _item_pos < _start_time or _item_end > _end_time then
-
-
-				--if it does not belong to same group_id(TODO check if upper part should be standalone and not nested in here and deactivated)
-				if _current_group_id ~= group_id then
-					--save mute state and mute items
-					overlapping_items[ov_index] = _item
-					overlapping_items_mute_state[ov_index] = reaper.GetMediaItemInfo_Value(_item, "B_MUTE")--get mute
-					reaper.SetMediaItemInfo_Value(_item, "B_MUTE", 1 )--mute that item
-					ov_index = ov_index + 1
-
-				else -- if belongs to same group
-					if _name ~= "/keep color" then   --change color if item is not marked for retaining color
-						if make_items_white == true then
-							--color to white
-							local white = reaper.ColorToNative(255,255,255)|0x1000000
-							ApplyColor_Items(white,_item)
-						else
-						--make item color brighter
-							MakeItemColorBrighter(_item)
-						end
-					end
-						--if they are all mono, set mono export, if not then stereo export (WARNING: daes not consider take envelopes, and it doesn't work)
-						-- if _take ~= nil then
-							-- local _pcm_source = reaper.GetMediaItemTake_Source(_take)
-							-- local channel_mode = reaper.GetMediaItemTakeInfo_Value(_take, "I_CHANMODE")
-							-- local num_channels = reaper.GetMediaSourceNumChannels(_pcm_source)
-							-- if channel_mode == 0 then 	--if normal
-								-- if num_channels > 1 then -- number of channels larger than 1
-									-- mono_item = false	 -- it's not mono
-								-- else
-									-- --natural mono file
-								-- end
-							-- else 						-- if not normal, but: reverse stereo, Mono(L+R), Mono L, Mono R
-								-- if channel_mode > 1 then  -- ife Mono(L+R) or Mono L or Mono R
-									-- --forced mono, great
-								-- end
-							-- end
-						-- end
-				end
-
-
-				--INACTIVE--end
-			end
-
-
-			--(DOES NOT WORK -- IT"S SHIT)
-			-- Msg(new_name)
-			-- Msg(mono_item)
-			-- Msg("==")
-			--Export mono or stereo
-			-- local output_script_name = "ChangeRenderSettingsToStereo"
-			-- if mono_item == true then
-				-- output_script_name = "ChangeRenderSettingsToMono"
-			-- end
-			-- --get script path
-			-- local info = debug.getinfo(1).source:match("@(.*)")
-			-- ofni = string.reverse(info)
-			-- idx = string.find(ofni, "\\" )
-			-- htap = string.sub(ofni, idx, -1)
-			-- path = string.reverse(htap)
-			-- --Msg(path);
-
-			-- batch_path = [["]]..path..output_script_name..[[.ahk"]]
-			-- io.popen(batch_path)
-		----------------------------------------------------------------------------------------------------
-
-		reaper.Main_OnCommand(41823,0) --File: Add project to render queue, using the most recent render settings
-
-		--recall old mute state
-			for j = 0, ov_index - 1 do
-				reaper.SetMediaItemInfo_Value(overlapping_items[j], "B_MUTE", overlapping_items_mute_state[j])--mute that item
-			end
-		----------------------------------------------------------------
-
-		--delete temp region
-		reaper.DeleteProjectMarker( 0, marker_index, true )
-
+		for_index = Get_ForIndex_by_MarkerIndex(marker_index)
+		dictionary[value] = clip_group[i]
+	
 		reaper.Main_OnCommand(40289,0) --Item: Unselect all items
-		reaper.Main_OnCommand(40020,0) --Time selection: Remove time selection and loop points
+	
+		--set original time selection
+		startOut, retval, endOut =  reaper.GetSet_LoopTimeRange( true, true, start_time, end_time, true)
+		
+	end
+
+	OgranizeRegions()
+
+end
+
+
+function add_regions_to_render_queue()
+	local master_track =  reaper.GetMasterTrack( 0 )
+	
+	--idi red po red
+	for i = 0, MAX_REGION_LEVEL do
+	
+		--stavi ceo red u render matrix
+		reaper.Main_OnCommand(40289,0) --Item: Unselect all items
+		for j in pairs (region_level[i]) do
+			--get region
+			local retval, isrgnOut, posOut, rgnendOut, nameOut, markrgnindexnumberOut = reaper.EnumProjectMarkers(region_level[i][j])
+			local item = dictionary[nameOut]
+			
+			--stavi regiju u matrix
+			if markrgnindexnumberOut ~= nil then
+				reaper.SetRegionRenderMatrix( 0, markrgnindexnumberOut, master_track, 1 )
+				
+				--selektuj sve label iteme u redu
+				if item ~= nil then
+					reaper.SetMediaItemSelected( item, true )				
+				end
+			end
+		end
+		
+		--selektuj sve iteme u redu
+		--soliraj sve iteme u grupi koji nisu mutirani
+		reaper.Main_OnCommand(40034,0) --Item grouping: Select all items in groups
+		reaper.Main_OnCommand(reaper.NamedCommandLookup("_BR_RESTORE_SOLO_MUTE_ALL_ITEMS_SLOT_1"),0) --SWS/BR: Restore items' mute state to all items, slot 01
+		
+		
+		--dodaj regije iz reda u render queue i iybrisi selekciju
+		if region_level[i][1] ~= nil then
+			reaper.Main_OnCommand(41823,0) --File: Add project to render queue, using the most recent render settings			
+		end
+		reaper.Main_OnCommand(40289,0) --Item: Unselect all items
+		for i = 0, all_markers_count -1 do
+			local retval, isrgnOut, posOut, rgnendOut, nameOut, markrgnindexnumberOut = reaper.EnumProjectMarkers(i)
+			if isrgnOut then
+				-- -- delete temp region
+				reaper.DeleteProjectMarker( 0, markrgnindexnumberOut, true )
+				reaper.SetRegionRenderMatrix(0,markrgnindexnumberOut,master_track,-1)
+			end
+		end
 	end
 
 	if active_project_type == "MADBOX" then
 		reaper.Main_OnCommand(41207,0) --render all
+	end
+end
+--save vertical mute state and mute all items which should not be rendered
+function update_region_mute_state(for_index,group_id)
+	--DEPRECATED, UNUSED, REGION MATRIX IS NOW USED
+	local retval, isrgnOut, posOut, rgnendOut, nameOut, markrgnindexnumberOut = reaper.EnumProjectMarkers(for_index)
+	startOut, retval, endOut =  reaper.GetSet_LoopTimeRange( true, true, posOut, rgnendOut, true)
+	
+	reaper.Main_OnCommand(40717,0) --Item: Select all items in current time selection
+	sel_count = reaper.CountSelectedMediaItems(0)
+
+	-- get time selection start and end time
+	_start_time = posOut
+	_end_time = rgnendOut
+
+	--take info from all items in vertical selection
+	local mono_item = true
+	for j = 0, sel_count - 1 do
+
+		--get stuff
+		local _item = reaper.GetSelectedMediaItem(0,j)
+		local _take = reaper.GetMediaItemTake(_item, 0)
+		local _name = ""
+		if _take ~= nil then
+			_name =  reaper.GetTakeName(_take)
+		else
+			_name = reaper.ULT_GetMediaItemNote(_item)
+			_name = string.gsub (_name, "\n", "")
+			_name = string.gsub (_name, "\r", "")
+		end
+
+		local _item_pos = reaper.GetMediaItemInfo_Value(_item,"D_POSITION")
+		local _item_len = reaper.GetMediaItemInfo_Value(_item,"D_LENGTH")
+		local _item_end = _item_pos + _item_len
+		local _current_group_id = reaper.GetMediaItemInfo_Value(_item,"I_GROUPID")
+
+		--INACTIVE -- if overlap item
+		--INACTIVE--if _item_pos < _start_time or _item_end > _end_time then
+
+
+		--if it does not belong to same group_id(TODO check if upper part should be standalone and not nested in here and deactivated)
+		if _current_group_id ~= group_id then
+			--save mute state and mute items
+			overlapping_items[ov_index] = _item
+			overlapping_items_mute_state[ov_index] = reaper.GetMediaItemInfo_Value(_item, "B_MUTE")--get mute
+			reaper.SetMediaItemInfo_Value(_item, "B_MUTE", 1 )--mute that item
+			ov_index = ov_index + 1
+
+		else -- if belongs to same group
+			if _name ~= "/keep color" then   --change color if item is not marked for retaining color
+				if make_items_white == true then
+					--color to white
+					local white = reaper.ColorToNative(255,255,255)|0x1000000
+					ApplyColor_Items(white,_item)
+				else
+				--make item color brighter
+					MakeItemColorBrighter(_item)
+				end
+			end
+		end
+	end
+	
+	
+	--recall old mute state
+	for j = 0, ov_index - 1 do
+		reaper.SetMediaItemInfo_Value(overlapping_items[j], "B_MUTE", overlapping_items_mute_state[j])--mute that item
 	end
 
 end
@@ -675,5 +703,224 @@ function post_export_dialog(message_title)
 end
 
 
+
+function OgranizeRegions() 
+	--get all markers count
+	all_markers_count, num_markersOut, num_regionsOut  = reaper.CountProjectMarkers( 0 )
+
+	--get time selection
+	start_time, end_time =  reaper.GetSet_LoopTimeRange2( 0, false, false, 0, 0, false)
+	delta_time = end_time - start_time
+	
+	
+	
+	if delta_time ~= 0 then
+		for k = 0, MAX_REGION_LEVEL do
+			for i = 0, all_markers_count - 1 do 
+				local retval, isrgnOut, posOut, rgnendOut, nameOut, markrgnindexnumberOut = reaper.EnumProjectMarkers2(0, i )
+				if isrgnOut and string.sub(nameOut,0,1) == "*" then		--ovde treba smisliti kako da ne ukljucuje obicne regije, preko imena ili boje
+					if posOut >= start_time and rgnendOut <= end_time then
+					local retval, isrgnOut, posOut, rgnendOut, nameOut, markrgnindexnumberOut = reaper.EnumProjectMarkers2(0, i ) -- i za ENUM
+						UpdateRegionLevel(i)
+					end
+				end
+			end	
+		end
+		
+	end
+	
+	
+	
+	for i = 0, MAX_REGION_LEVEL do
+		for k in pairs (region_level[i]) do
+			local retval, isrgnOut, posOut, rgnendOut, nameOut, markrgnindexnumberOut = reaper.EnumProjectMarkers(region_level[i][k])
+			local original_name = string.sub(nameOut,2,-1)
+			-- Msg(original_name)
+			reaper.SetProjectMarker2(0,markrgnindexnumberOut,true,posOut,rgnendOut,original_name)
+		end
+	end
+	-- PrintLevelState()
+end
+
+
+function Get_ForIndex_by_MarkerIndex(marker_index)	--forIndex is for all regions and markers together, marker_index is region's or marker's specific index
+	local all_markers_count, num_markersOut, num_regionsOut  = reaper.CountProjectMarkers( 0 )
+	for i=0, all_markers_count-1 do
+		local retval, isrgnOut, posOut, rgnendOut, nameOut, markrgnindexnumberOut = reaper.EnumProjectMarkers(i)
+		if markrgnindexnumberOut == marker_index then
+			for_index = i
+			break
+		end
+	end
+	return for_index
+end
+
+
+function PrintLevelState()
+	Msg("")
+	Msg("")
+	for i = 0, MAX_REGION_LEVEL do
+		Msg([[REGION LEVEL ]]..i..[[:]])
+		for k in pairs (region_level[i]) do
+			 local retval, isrgnOut, posOut, rgnendOut, nameOut, markrgnindexnumberOut = reaper.EnumProjectMarkers2(0, region_level[i][k])
+			 GetRegionLevel(region_level[i][k])
+			 Msg(region_level[i][k].."         "..nameOut)
+			 Msg(dictionary[nameOut])
+		end
+	end
+end
+
+level = {}
+level[0] = reaper.ColorToNative(200,150,100)|0x1000000
+level[1] = reaper.ColorToNative(0  ,150,150)|0x1000000
+level[2] = reaper.ColorToNative(200,200,0  )|0x1000000
+level[3] = reaper.ColorToNative(200,0  ,100)|0x1000000
+level[4] = reaper.ColorToNative(0  ,0  ,100)|0x1000000
+level[5] = reaper.ColorToNative(60 ,30 ,100)|0x1000000
+level[6] = reaper.ColorToNative(80 ,100,100)|0x1000000
+level[7] = reaper.ColorToNative(0  ,0  ,200)|0x1000000
+level[8] = reaper.ColorToNative(200,150,100)|0x1000000
+level[9] = reaper.ColorToNative(0  ,150,150)|0x1000000
+level[10]= reaper.ColorToNative(200,200,0  )|0x1000000
+level[11]= reaper.ColorToNative(200,0  ,100)|0x1000000
+level[12]= reaper.ColorToNative(0  ,0  ,100)|0x1000000
+level[13]= reaper.ColorToNative(60 ,30 ,100)|0x1000000
+level[14]= reaper.ColorToNative(80 ,100,100)|0x1000000
+level[15]= reaper.ColorToNative(0  ,0  ,200)|0x1000000
+
+--init nizova
+region_level = {}
+MAX_REGION_LEVEL = 14 --OD NULA KREĆE
+for i = 0,MAX_REGION_LEVEL do		
+	region_level[i] = {}
+end
+-----
+	
+
+function setLevel(for_index,layer)
+	--ubaci ako već nije na listi
+	local not_on_list = true
+	if region_level[layer] ~= nil then
+		for k in pairs (region_level[layer]) do
+			if region_level[layer][k] == for_index then
+				not_on_list = false
+				break
+			end
+		end
+		if not_on_list then
+			table.insert(region_level[layer],for_index)
+			local retval, isrgnOut, posOut, rgnendOut, nameOut, markrgnindexnumberOut = reaper.EnumProjectMarkers2(0, for_index ) -- i za ENUM
+			if string.sub(nameOut,0,1) ~= "*" then
+				new_name = "*"..nameOut
+			else
+				new_name = nameOut
+			end
+			reaper.SetProjectMarker3(0,markrgnindexnumberOut,isrgnOut,posOut,rgnendOut,new_name,level[layer])
+		end
+	end
+end
+
+function removeFromLevel(for_index,layer)
+	--find element and delete it, duplicates also
+	for k in pairs (region_level[layer]) do
+		if region_level[layer][k] == for_index then
+			table.remove(region_level[layer],k)
+		end
+	end	
+
+	
+end
+
+
+
+function UpdateRegionLevel(for_index)
+	--get this region info
+	local retval, isrgnOut, posOut, rgnendOut, nameOut, markrgnindexnumberOut = reaper.EnumProjectMarkers(for_index) -- i za ENUM
+	-- Msg("")
+	--get current region level
+	local this_region_level = GetRegionLevel(for_index)
+	-- Msg("UpdateRegionLevel for:"..nameOut..[[(]]..this_region_level..[[)]])
+	
+	--find if there is free space in lower levels, starting from level 0 and then break when you find free space
+	if this_region_level ~= "UNKNOWN" then
+		for i = 0, this_region_level do
+			-- Msg("Comparing level:"..i)
+			this_level_is_blocked = false
+			for k in pairs (region_level[i]) do
+				local cValue, cIsRegion, cPos, cEnd, cName, cMarkerIndex = reaper.EnumProjectMarkers(region_level[i][k])
+				if  markrgnindexnumberOut ~= cMarkerIndex then--do not compare with self
+					if rgnendOut > cPos then							--proveri samo ako je regija levo od kraja trenutne
+						-- Msg("Comparing:"..nameOut.." with "..cName)
+						if posOut < cEnd  then --ako se preklapaju 	--ovde mozda <=	
+							-- Msg("OOPS, REGIONS OVERLAP")
+							this_level_is_blocked = true
+							if i == this_region_level then--ako se overlapuje se onom regijom koja je istom nivou
+								if posOut > cPos then
+									-- Msg("Move to new level")
+									removeFromLevel(for_index, this_region_level) --prebaci ga na sledeci level
+									this_region_level = this_region_level + 1
+									break
+								elseif posOut == cPos then
+									if markrgnindexnumberOut > cMarkerIndex then
+										removeFromLevel(for_index, this_region_level) --prebaci ga na sledeci level
+										this_region_level = this_region_level + 1
+									end
+								else
+									-- Msg("leave it here")
+								end
+							else
+								-- Msg("Continue Searching")
+							end
+						else
+							-- Msg("No overlap")
+						end
+					else
+						-- Msg("Region "..cName.." is right of region "..nameOut) --ŠTA AKO SE PREKLAPA SA TE STRANE NA ISTOM LEVELU ?
+					end
+				else
+					-- Msg("Don't compare with self")
+				end
+			end
+			--proveri stanje nakon provere ovog levela
+			if this_level_is_blocked == false then
+				-- Msg("Space is free on level:"..i)
+				-- Msg("Setting new Level")
+				removeFromLevel(for_index,this_region_level) --izrbisi ga sa trenutne pozicije
+				this_region_level = i
+				break
+			end
+		end
+	else
+		setLevel(for_index,0) --first time setting
+	end
+	
+	--set new level
+	setLevel(for_index,this_region_level)
+	
+end
+
+
+
+
+function GetRegionLevel(for_index)
+	local retval, isrgnOut, posOut, rgnendOut, nameOut, markrgnindexnumberOut = reaper.EnumProjectMarkers(for_index) -- i za ENUM
+	result = "UNKNOWN"
+	for i = 0, MAX_REGION_LEVEL do
+		for k in pairs (region_level[i]) do
+			if region_level[i][k] == for_index then
+				result =  i
+				break
+			end
+		end
+	end
+	-- Msg(nameOut..[[ is on level: ]]..result)
+	return result
+end
+
+
+
+
 --EXECUTION=================================================================================================================================================================
-Main()--run
+-- reaper.PreventUIRefresh( 1 )
+Main() -- run script
+-- reaper.PreventUIRefresh( -1 )
