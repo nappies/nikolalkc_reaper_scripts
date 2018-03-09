889a1eabc9dbff4402d7c3bfdfa0ea0c5c6406ba
nikolalkc <nikolalkc@hotmail.com>
2018-03-09 10:57:38 +0100

    @ tags

--- MHG/nikolalkc_MHG - Project Template 16FX 4ATM OGG.lua
+++ MHG/nikolalkc_MHG - Project Template 16FX 4ATM OGG.lua
@@ -1,13 +1,11 @@
 --[[
- ReaScript Name: MHG Project Template 16FX 4ATM OGG
- Author: nikolalkc
- Repository URL: https://github.com/nikolalkc/nikolalkc_reaper_scripts
- REAPER: 5+
- Extensions: SWS
- Version: 1.0
- Provides: [projecttpl] MHG_Template_16FX_4ATM_2REV_OGG_30fps.rpp
- MetaPackage:
- About:
+ @description MHG Project Template 16FX 4ATM OGG
+ @author nikolalkc
+ @link https://github.com/nikolalkc/nikolalkc_reaper_scripts
+ @version 1.0
+ @provides [projecttpl] MHG_Template_16FX_4ATM_2REV_OGG_30fps.rpp
+ @metapackage
+ @about
   Template for game audio projects, updated to use render matrix as preset
   
 ]]
